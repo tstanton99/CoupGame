e29d5c7e6cb7323a52823c28ef3c4110a4e38dd2
TomWija <tomwi@brandwatch.com>
2018-10-08 15:21:34 +0100

    :tada:

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import GameBoard from './GameBoard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<GameBoard />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/GameBoard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameBoard extends Component {
+  render() {
+    return (
+      <div className="GameBoard">
+          This will be a gameboard
+      </div>
+    );
+  }
+}
+
+export default GameBoard;
